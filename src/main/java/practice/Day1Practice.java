0f716676c017b297fda65adbbc616cc00a55f7c2
sudhanshu26ranjan <sudha.ranjan26@gmail.com>
2026-05-20 12:27:19 +0200

    Initial Java practice setup

---
+++ src/main/java/practice/Day1Practice.java
@@ -0,0 +1,8 @@
+package practice;
+
+//29.05.2026
+public class Day1Practice {
+    public static void main(String[] args) {
+        System.out.println("Java comeback journey started for Sudhanshu!");
+    }
+}
